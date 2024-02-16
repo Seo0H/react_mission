76a966d00ab62c4860fac9b553b5d07ea1994a61
seo <seo0h9@gmail.com>
2024-02-17 02:10:07 +0900

    [RMIS-20] chore : api factory 구현
    - api fetching 에러 처리, 진행 상태와 같은 공통 로직 처리를 규격화 하기 위한 api factory class 구현

---
+++ src/api/factory.ts
@@ -0,0 +1,56 @@
+import { initialApiStatus, type API, type APIStatusType, type ErrorType } from '@/api/type';
+
+export default class APIFactory<APIResponse> implements API<APIResponse> {
+  endPoint: string;
+
+  private status: APIStatusType = initialApiStatus;
+  readonly abortSignal?: AbortSignal;
+
+  constructor(endPoint: string, abortSignal?: AbortSignal) {
+    this.endPoint = endPoint;
+    if (abortSignal) this.abortSignal = abortSignal;
+  }
+
+  async fetch(init?: RequestInit | undefined): Promise<APIResponse | ErrorType> {
+    try {
+      this.status = { ...initialApiStatus, isLoading: true };
+
+      const response = await fetch(this.endPoint, { ...init, signal: this.abortSignal });
+
+      if (!response.ok) {
+        throw new Response('Something Wrong', { status: response.status });
+      }
+
+      const data = await response.json();
+
+      this.status = { ...initialApiStatus, isSuccess: true };
+
+      return data;
+    } catch (e) {
+      if (this.abortSignal?.aborted) {
+        this.status = { ...initialApiStatus, isAbort: true };
+
+        return {
+          message: 'ABORTED',
+          error: e,
+        };
+      }
+
+      if (e instanceof Error) {
+        this.status = { ...initialApiStatus, isError: true };
+
+        throw {
+          message: e.message,
+          error: e,
+        };
+      }
+
+      this.status = { ...initialApiStatus, isError: true };
+
+      throw e;
+    }
+  }
+  getStatus(): APIStatusType {
+    return this.status;
+  }
+}
