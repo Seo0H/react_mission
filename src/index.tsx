cc10e439b0ba78193be4c9a22055dcea7fefe6a9
seo <seo0h9@gmail.com>
2024-02-17 02:10:54 +0900

    [RMIS-20] feat : router, loader 구현

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 
 import ReactDOM from 'react-dom/client';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 
-import App from '@/views';
+import { routes } from '@/routes';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+const router = createBrowserRouter(routes);
 
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 );
---
+++ src/types/response.ts
@@ -0,0 +1,54 @@
+type FormType = 'text' | 'number' | 'checkbox' | 'radio' | 'radioNumber' | 'radioWithInput';
+type TargetType = number | [min: number, max: number] | string;
+type Validate =
+  | {
+      type: 'not';
+      target: '' | '-1' | [];
+    }
+  | {
+      type: 'minMax';
+      target: [min: number, max: number];
+    }
+  | {
+      type: 'sameAs';
+      target: string | number;
+    }
+  | {
+      type: 'pattern';
+      target: string;
+    }
+  | {
+      type: 'minMaxLength';
+      target: [min: number, max: number];
+      validateText: string;
+    };
+
+type ValidateType = Validate['type'];
+
+export type FormAPIResponseType = {
+  status: number;
+  data: {
+    userId?: string; // userId는 typeID가 common인 특수한 경우에만 리턴된다.
+    forms: {
+      name: string; // form 작성 후 서버에 보낼 때의 이름
+      question: string; // user에게 보여줘야 하는 질문
+      required: boolean; // 필수 질문 여부
+      type: FormType; // form type;
+      placeholder:
+        | string
+        | number
+        | boolean
+        | {
+            label: string;
+            value: string;
+            checked: boolean;
+          }[]; // user가 선택 전 default로 선택된 value;
+      validate: Validate[];
+    }[];
+    escapeValidate: {
+      name: string;
+      type: ValidateType;
+      target: TargetType;
+    }[]; // if name에 해당하는 value가 있고, 해당 validate를 통과하지 못하면 `no_target`으로 리다이렉트 시킨다.
+  };
+};
