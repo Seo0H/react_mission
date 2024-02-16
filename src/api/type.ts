588fd1e4062f01f7b9303fe9eafc65564d93b51f
seo <seo0h9@gmail.com>
2024-02-17 02:10:07 +0900

    [RMIS-20] chore : api factory 구현
    - api fetching 에러 처리, 진행 상태와 같은 공통 로직 처리를 규격화 하기 위한 api factory class 구현

---
+++ src/api/type.ts
@@ -0,0 +1,29 @@
+export type APIStatusType = {
+  isLoading: boolean;
+  isSuccess: boolean;
+  isError: boolean;
+  isAbort: boolean;
+};
+
+export interface API<APIResponseType> {
+  readonly endPoint: string;
+  fetch(): Promise<APIResponseType | ErrorType>;
+  getStatus(): APIStatusType;
+}
+
+export type ErrorType = {
+  message: string | undefined;
+  error: unknown;
+};
+
+export const initialApiStatus: APIStatusType = {
+  isLoading: false,
+  isSuccess: false,
+  isError: false,
+  isAbort: false,
+};
+
+export function isError(res: unknown | ErrorType): res is ErrorType {
+  if (typeof res === 'object' && res !== null && 'error' in res) return true;
+  return false;
+}
