9bbfa4698ed595abd771c0a753d2a9a2290f4ec9
seo <seo0h9@gmail.com>
2024-02-20 00:58:14 +0900

    [RMIS-22] feat : getQuestionWithId api 추가

--- src/api/form/index.ts
+++ src/api/form/index.ts
@@ -1,5 +1,5 @@
 import APIFactory from '@/api/factory';
-import { FormAPIResponseType } from '@/api/types/response';
+import { FormAPIResponseType } from '@/api/types/server-response';
 
 const getCommonQuestion = async () => {
   const client = new APIFactory<FormAPIResponseType>('/api/question/common/index.json');
@@ -7,6 +7,13 @@ const getCommonQuestion = async () => {
   return data;
 };
 
+const getQuestionWithId = async (id: number) => {
+  const client = new APIFactory<FormAPIResponseType>(`/api/question/${id}/index.json`);
+  const data = await client.fetch();
+  return data;
+};
+
 export const formAPI = {
   getCommonQuestion,
+  getQuestionWithId,
 };
