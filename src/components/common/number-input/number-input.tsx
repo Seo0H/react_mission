979cd6d26d1345f1bc3af7fb0e3e64fdeee9d67e
seo <seo0h9@gmail.com>
2024-02-19 15:51:18 +0900

    [RMIS-22] fix : input, number input 컴포넌트 확장성 개선

--- src/components/common/number-input/number-input.tsx
+++ src/components/common/number-input/number-input.tsx
@@ -1,8 +1,6 @@
 import { ComponentPropsWithRef, forwardRef } from 'react';
 
-interface InputOptions {}
-
-interface NumberInputProps extends InputOptions, Omit<ComponentPropsWithRef<'input'>, 'type'> {}
+interface NumberInputProps extends Omit<ComponentPropsWithRef<'input'>, 'type'> {}
 
 export const NumberInput = forwardRef<HTMLInputElement, NumberInputProps>(function NumberInput(props, ref) {
   return <input type='number' {...props} ref={ref} />;
