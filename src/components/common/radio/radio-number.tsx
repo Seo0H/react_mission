1dc923352891eb6e203a6661e3cb14e520145dab
seo <seo0h9@gmail.com>
2024-02-20 00:57:46 +0900

    [RMIS-22] docs : 주석 추가

--- src/components/common/radio/radio-number.tsx
+++ src/components/common/radio/radio-number.tsx
@@ -3,19 +3,42 @@ import { ChangeEvent } from 'react';
 import { Radio } from '@/components/common/radio/radio';
 import { RadioGroup } from '@/components/common/radio/radio-group';
 
-interface RadioNumberProps {
+interface RadioNumberOptions {
+  /**
+   * 라디오 넘버의 시작 점수
+   */
   minScore: number;
+
+  /**
+   * 라디오 넘버의 끝 점수
+   */
   maxScore: number;
+
+  /**
+   * 각 점수의 일정한 차이
+   */
   step?: number;
-  value?: number;
+
+  /**
+   * 기본 값
+   */
+  defaultValue?: number;
+
+  /**
+   * 라디오 넘버의 이름
+   */
   name?: string;
+
+  /**
+   * 'Checkbox'의 선택된 상태가 변경될 때 호출되는 콜백
+   */
   onChange?: (e: ChangeEvent<HTMLInputElement>) => void;
 }
 
-export const RadioNumber = ({ minScore, maxScore, step = 1, onChange, value, name }: RadioNumberProps) => {
+export const RadioNumber = ({ minScore, maxScore, step = 1, onChange, defaultValue, name }: RadioNumberOptions) => {
   const radioNumberContext = Array.from({ length: (maxScore - minScore) * step + 1 }).map((_, idx) => (idx + 1) * step);
   return (
-    <RadioGroup value={String(value)} onChange={onChange} name={name}>
+    <RadioGroup onChange={onChange} name={name} defaultValue={String(defaultValue)}>
       {radioNumberContext.map((value) => (
         <Radio value={String(value)} key={crypto.randomUUID()}>
           {value} 점
