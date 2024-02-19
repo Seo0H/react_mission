b75258b89c6e1f892db29510763ef4164fea88e1
seo <seo0h9@gmail.com>
2024-02-20 00:48:39 +0900

    [RMIS-22] feat : 조건부 랜더링 컴포넌트에 input type 별 컴포넌트 적용

--- src/components/form/conditional-input.tsx
+++ src/components/form/conditional-input.tsx
@@ -1,21 +1,29 @@
 import { type ComponentPropsWithRef, type ReactElement, type ChangeEvent } from 'react';
 
+import DefaultCheckbox from '@/components/common/checkbox/checkbox-defult';
 import { Input } from '@/components/common/input';
 import { NumberInput } from '@/components/common/number-input';
+import DefaultRadio from '@/components/common/radio/radio-default';
+import { RadioWithInput } from '@/components/common/radio/radio-input';
 import { RadioNumber } from '@/components/common/radio/radio-number';
 import { isMultiInputAnswer } from '@/components/form/utils';
+import { isArray } from '@/utils/is';
 
-import type { MultiInputAnswer, SingleInputAnswer } from '@/api/types/request';
-import type { Form } from '@/api/types/response';
+import type { Form, FormType, Selection } from '@/api/types/server-response';
 
-type InputComponent<
-  T = Omit<ComponentPropsWithRef<'input'>, 'value'> & { value: SingleInputAnswer | MultiInputAnswer },
-> = (props: T) => ReactElement;
+type Omitted = 'value' | 'value' | 'onChange';
+
+type InputComponent<T = Omit<ComponentPropsWithRef<'input'>, Omitted> & Omit<ConditionalInputProps, 'type'>> = (
+  props: T,
+  type: FormType,
+) => ReactElement;
 
 interface ConditionalInputProps {
+  name: string;
   type: Form['type'];
   onChange: (e: ChangeEvent<HTMLInputElement>) => void;
-  value: SingleInputAnswer | MultiInputAnswer;
+  value: string;
+  selections?: Selection[];
 }
 
 const inputComponents: { [key in Form['type']]: InputComponent } = {
@@ -27,19 +35,29 @@ const inputComponents: { [key in Form['type']]: InputComponent } = {
     if (isMultiInputAnswer(value)) throw new Error('Number Input must have single input answer value');
     return <NumberInput value={Number(value)} {...rest} />;
   },
-  checkbox: () => <></>,
-  radio: (props) => <></>,
+  checkbox: ({ value, selections, ...rest }) => {
+    if (!isSelection(selections)) throw new Error('Radio must have multi answer value');
+    return <DefaultCheckbox contexts={selections} value={String(value)} {...rest} />;
+  },
+  radio: ({ value, selections, ...rest }) => {
+    if (!isSelection(selections)) throw new Error('Radio must have multi answer value');
+    return <DefaultRadio contexts={selections} value={String(value)} {...rest} />;
+  },
   radioNumber: ({ onChange, value, name }) => (
-    <RadioNumber onChange={onChange} value={Number(value)} name={name} maxScore={7} minScore={1} />
+    <RadioNumber onChange={onChange} defaultValue={Number(value)} name={name} maxScore={7} minScore={1} />
   ),
-  radioWithInput: () => <></>,
+  radioWithInput: ({ value, selections, ...rest }) => {
+    if (!isSelection(selections)) throw new Error('Radio Input must have multi answer value');
+    return <RadioWithInput context={selections} value={String(value)} {...rest} />;
+  },
 };
 
-const ConditionalInput = ({
-  type,
-  ...rest
-}: ConditionalInputProps & Omit<ComponentPropsWithRef<'input'>, 'value' | 'onChange'>) => {
-  return <div>{inputComponents[type](rest)}</div>;
+const ConditionalInput = ({ type, ...rest }: ConditionalInputProps & Omit<ComponentPropsWithRef<'input'>, Omitted>) => {
+  return <div>{inputComponents[type](rest, type)}</div>;
 };
 
 export default ConditionalInput;
+
+function isSelection(selections: unknown): selections is Selection[] {
+  return !!selections && isArray(selections);
+}
