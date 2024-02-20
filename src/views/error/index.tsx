8df248dc91577aa3ca1ef50f953b9f28e5e38ffc
seo <seo0h9@gmail.com>
2024-02-20 17:12:32 +0900

    [RMIS-26] feat : error page 홈으로 돌아가기 추가

--- src/views/error/index.tsx
+++ src/views/error/index.tsx
@@ -1,5 +1,11 @@
+import { Link } from 'react-router-dom';
+
 const ErrorPage = () => {
-  return <div>Error Something..</div>;
+  return (
+    <div>
+      Error Something..<Link to='/'>홈으로 돌아가기</Link>
+    </div>
+  );
 };
 
 export default ErrorPage;
