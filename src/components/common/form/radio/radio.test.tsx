9f06914f01efdc764f131b100b76fa02d21af45c
seo <seo0h9@gmail.com>
2024-02-23 13:08:30 +0900

    [RMIS-33] test : radio와 radio group 테스트 분리

--- src/components/common/form/radio/radio.test.tsx
+++ src/components/common/form/radio/radio.test.tsx
@@ -3,13 +3,12 @@ import React from 'react';
 import { fireEvent, render, screen } from '@testing-library/react';
 
 import { Radio } from '@/components/common/form/radio/radio';
-import { RadioGroup } from '@/components/common/form/radio/radio-group';
 
 afterEach(() => {
   jest.restoreAllMocks();
 });
 
-describe('Radio : 전달된 prop이 컴포넌트에 의도한대로 적용되어야 한다.', () => {
+describe('Radio 컴포넌트 테스트', () => {
   it('name, value, id prop test', () => {
     const utils = render(<Radio name='name' value='' id='id' data-testid='input' />);
 
@@ -28,7 +27,9 @@ describe('Radio : 전달된 prop이 컴포넌트에 의도한대로 적용되어
     expect(input).toBeChecked();
     expect(input).toBeRequired();
   });
+});
 
+describe('Radio 동작 테스트', () => {
   it('제어 테스트', async () => {
     const mock = jest.fn();
 
@@ -64,42 +65,5 @@ describe('Radio : 전달된 prop이 컴포넌트에 의도한대로 적용되어
 
     expect(one).toBeChecked();
     expect(mock).toHaveBeenCalledTimes(1);
-  });
-});
-
-describe('RadioGroup : 전달된 prop이 컴포넌트에 의도한대로 적용되어야 한다.', () => {
-  it('defaultValue prop 작동 테스트', () => {
-    const utils = render(
-      <RadioGroup defaultValue='b'>
-        <Radio value='a'>a</Radio>
-        <Radio value='b'>b</Radio>
-      </RadioGroup>,
-    );
-
-    expect(utils.getByLabelText('b')).toBeChecked();
-  });
-  it('disabled prop 작동 테스트', () => {
-    // disabled test
-    const Component = () => (
-      <RadioGroup isDisabled>
-        <Radio value='one' data-testid='input'>
-          One
-        </Radio>
-        <Radio value='two' isDisabled data-testid='input'>
-          Two
-        </Radio>
-        <Radio value='three' isDisabled={false} data-testid='input'>
-          Three
-        </Radio>
-      </RadioGroup>
-    );
-
-    const utils = render(<Component />);
-
-    const [firstRadio, secondRadio, thirdRadio] = utils.getAllByTestId('input');
-
-    expect(firstRadio).toBeDisabled();
-    expect(secondRadio).toBeDisabled();
-    expect(thirdRadio).not.toBeDisabled();
   });
 });
