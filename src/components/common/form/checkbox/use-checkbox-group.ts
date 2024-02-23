e8dbf1578cb7b8cba2d344c321941c4f61d41cd9
seo <seo0h9@gmail.com>
2024-02-23 11:31:59 +0900

    [RMIS-33] fix : default checkbox 선택 오류 로직 수정
    - Checkbox 컴포넌트 내부의 useCheckboxGroup의 group values 를 검증하는 과정에서 group values가 기본값이  빈 배열이였기에 해당 문제가 발생. 기본값을undefined로 변경하고 검증 로직 수정

--- src/components/common/form/checkbox/use-checkbox-group.ts
+++ src/components/common/form/checkbox/use-checkbox-group.ts
@@ -5,7 +5,7 @@ import { isInputEvent } from '@/components/common/form/utils';
 
 export interface UseCheckboxGroupProps {
   values?: string[];
-  defaultValue?: string;
+  defaultValue?: string[];
   onChange?(prop: OnChangeProp): void;
   isDisabled?: boolean;
   name?: string;
@@ -14,7 +14,9 @@ export interface UseCheckboxGroupProps {
 export const useCheckboxGroup = (props: UseCheckboxGroupProps = {}) => {
   const { onChange: onChangeProp, values: valuesProp, defaultValue, name: nameProp, isDisabled, ...htmlProps } = props;
 
-  const [valuesState, setValues] = useState<string[]>(defaultValue === undefined ? [] : [defaultValue]);
+  const [valuesState, setValues] = useState<string[] | undefined>(
+    defaultValue === undefined ? undefined : [...defaultValue],
+  );
   const isControlled = valuesProp !== undefined;
   const values = isControlled ? valuesProp : valuesState;
 
@@ -30,11 +32,11 @@ export const useCheckboxGroup = (props: UseCheckboxGroupProps = {}) => {
       const nextValue = isInputEvent(eventOrValue) ? eventOrValue.target.value : eventOrValue;
 
       if (!isControlled) {
-        const isHasValue = valuesState.find((value) => value === nextValue);
+        const isHasValue = valuesState?.find((value) => value === nextValue);
 
-        if (typeof isHasValue === 'string') {
-          setValues((prevValues) => prevValues.filter((value) => value !== nextValue));
-        } else setValues((prevValues) => [...prevValues, nextValue]);
+        if (typeof isHasValue === 'string' && Array.isArray(valuesState)) {
+          setValues(valuesState.filter((value) => value !== nextValue));
+        } else if (Array.isArray(valuesState)) setValues([...valuesState, nextValue]);
       }
 
       if (isInputEvent(eventOrValue)) {
