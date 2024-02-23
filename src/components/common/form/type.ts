0c647dff5cb8c219be051b2dc66937d517b414ed
seo <seo0h9@gmail.com>
2024-02-23 11:25:42 +0900

    [RMIS-33] refactor : QuestionContext type 분리

--- src/components/common/form/type.ts
+++ src/components/common/form/type.ts
@@ -1,3 +1,5 @@
 export type OnChangeProp = string | React.ChangeEvent<HTMLInputElement>;
 
 export type EventOrValue = React.ChangeEvent<HTMLInputElement> | string;
+
+export type QuestionContext = { value: string; label: string };
