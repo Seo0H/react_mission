10753a1a8d158e420a11d7dd2e941d7547d5c063
seo <seo0h9@gmail.com>
2024-02-23 13:16:00 +0900

    [RMIS-33] test : 중복 테스트 삭제, 테스트 설명 구체화

--- src/components/common/form/checkbox/checkbox.test.tsx
+++ src/components/common/form/checkbox/checkbox.test.tsx
@@ -2,7 +2,7 @@ import { render } from '@testing-library/react';
 
 import { Checkbox } from '@/components/common/form/checkbox';
 
-test('Checkbox props 전달 테스트', () => {
+test('Checkbox의 value, id, name 및 추가 props가 적절히 전달되고 props에 맞는 기능이 작동해야 한다.', () => {
   const utils = render(<Checkbox value='test' id='id' name='name' data-testid='checkbox' />);
 
   let checkbox = utils.getByTestId('checkbox');
