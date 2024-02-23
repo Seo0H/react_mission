b9b920c23b5eff9fd1e43d282f8a5c31c78bb9c6
seo <seo0h9@gmail.com>
2024-02-23 13:16:00 +0900

    [RMIS-33] test : 중복 테스트 삭제, 테스트 설명 구체화

--- src/components/common/form/checkbox/checkbox-group.test.tsx
+++ src/components/common/form/checkbox/checkbox-group.test.tsx
@@ -1,29 +1,35 @@
-import { render } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 
 import { CheckBoxGroup, Checkbox } from '@/components/common/form/checkbox';
 
-test('Checkbox Group Test', async () => {
-  const utils = render(
-    <CheckBoxGroup isDisabled values={['two', 'three']}>
-      <Checkbox data-testid='input' value='one'>
-        One
-      </Checkbox>
-      <Checkbox data-testid='input' value='two'>
-        Two
-      </Checkbox>
-      <Checkbox data-testid='input' value='three' isDisabled={false}>
-        Three
-      </Checkbox>
-    </CheckBoxGroup>,
-  );
+const runTest = () => {
+  const one = screen.getByLabelText('One');
+  const two = screen.getByLabelText('Two');
+  const three = screen.getByLabelText('Three');
 
-  const [firstCheckbox, secondCheckbox, thirdCheckbox] = await utils.findAllByTestId('input');
+  expect(one).toBeChecked();
+  expect(two).not.toBeChecked();
+  expect(three).not.toBeChecked();
 
-  expect(firstCheckbox).not.toBeChecked();
-  expect(secondCheckbox).toBeChecked();
-  expect(thirdCheckbox).toBeChecked();
+  fireEvent.click(two);
 
-  expect(firstCheckbox).toBeDisabled();
-  expect(secondCheckbox).toBeDisabled();
-  expect(thirdCheckbox).not.toBeDisabled();
+  expect(one).toBeChecked();
+  expect(two).toBeChecked();
+  expect(three).not.toBeChecked();
+};
+
+describe('Checkbox Group', () => {
+  test('체크박스 그룹에서 초기값 적용, 다중 선택이 되어야 한다.', async () => {
+    const onChange = jest.fn();
+    render(
+      <CheckBoxGroup name='checkbox' defaultValue={['1']} onChange={onChange}>
+        <Checkbox value='1'>One</Checkbox>
+        <Checkbox value='2'>Two</Checkbox>
+        <Checkbox value='3'>Three</Checkbox>
+      </CheckBoxGroup>,
+    );
+
+    runTest();
+    expect(onChange.mock.lastCall[0].target.value).toBe('2'); // to be receive event
+  });
 });
