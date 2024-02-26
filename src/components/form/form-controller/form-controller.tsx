557220ed3e6990789f482f3404977c7fb9cd1241
seo <seo0h9@gmail.com>
2024-02-26 20:01:43 +0900

    [RMIS-14] refactor : 추상화 정도 통일을 위한 form-controller 계층 병합

--- src/components/form/form-controller/form-controller.tsx
+++ src/components/form/form-controller/form-controller.tsx
@@ -1,4 +1,8 @@
-import { FormQuestion } from '@/components/form';
+import { Button } from '@/components/common/buttons';
+import ErrorMessage from '@/components/common/error/message';
+
+import { ConditionalInput } from '@/components/form';
+import { useFormContext } from '@/hooks/use-form/form-context';
 
 import styles from './form-controller.module.css';
 import { useFormSubmit } from './hooks/use-form-control';
@@ -7,15 +11,28 @@ import { useFormQuestionControl } from './hooks/use-form-question-number';
 export const FormController = () => {
   const { isLastQuestion, handleClick, handleKeyDown, resetIdx, form } = useFormQuestionControl();
   const { onSubmit } = useFormSubmit({ cleanUp: resetIdx });
+  const { getFieldState } = useFormContext();
+
+  const { name, question, required, ...rest } = form;
+
+  const { invalid, error } = getFieldState(name);
   const buttonType = isLastQuestion ? 'submit' : 'button';
 
   return (
     <form onSubmit={onSubmit} onKeyDown={handleKeyDown} className={styles['form-container']}>
-      <FormQuestion {...{ form }} />
+      <label htmlFor={name} className={`${styles['label']} ${required && styles['required']}`}>
+        {question}
+      </label>
 
-      <button key={buttonType} type={buttonType} onClick={handleClick}>
+      <div className={styles['input-wrapper']}>
+        <ConditionalInput key={form.name} selections={form.radioContext} {...rest} {...form} />
+
+        <div className={styles['invalid-message-wrapper']}>{invalid && <ErrorMessage error={error?.message} />}</div>
+      </div>
+
+      <Button key={buttonType} type={buttonType} onClick={handleClick}>
         {isLastQuestion ? `제출` : `다음`}
-      </button>
+      </Button>
     </form>
   );
 };
--- src/components/form/index.ts
+++ src/components/form/index.ts
@@ -1,3 +1,2 @@
-export { FormQuestion } from './question/question';
 export { ConditionalInput } from './conditional-input/conditional-input';
 export { inputComponents } from './conditional-input/input-components';
