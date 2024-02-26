1faaf50451918729cf3c7035b3d8f3ab1ba0882d
seo <seo0h9@gmail.com>
2024-02-26 16:14:16 +0900

    delete : loaders 의 불필요한 try-catch 문 제거

--- src/routes/loaders.ts
+++ src/routes/loaders.ts
@@ -4,17 +4,12 @@ import { makeClientForm } from '@/routes/utils';
 
 export const loaders = {
   async mainPage(params: string) {
-    try {
-      const response = await formAPI.getQuestionWithId(params);
-      if (isError(response)) throw new Error('error');
+    const response = await formAPI.getQuestionWithId(params);
+    if (isError(response)) throw new Error('error');
 
-      return {
-        ...response,
-        data: { ...response.data, forms: makeClientForm(response.data.forms) },
-      };
-    } catch (e) {
-      console.log(e);
-      throw new Error('question data fetching 중 에러 발생');
-    }
+    return {
+      ...response,
+      data: { ...response.data, forms: makeClientForm(response.data.forms) },
+    };
   },
 };
