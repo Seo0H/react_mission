ad45d3745037aab9f0ddbdec219b78e34863667d
seo <seo0h9@gmail.com>
2024-02-29 17:33:21 +0900

    [RMIS-39] fix : default radio 가 use form field 값에 적용되지 않는 현상 수정

--- src/hooks/use-form/logic/get-field-value.ts
+++ src/hooks/use-form/logic/get-field-value.ts
@@ -1,5 +1,9 @@
 import { isUndefined } from '@/utils/is';
 
+import { isCheckBoxInput, isRadioInput } from '../utils/is';
+
+import { getCheckboxValue } from './get-checkbox-value';
+import { getRadioValue } from './get-radio-value';
 import type { Field } from '../types/fields';
 
 export function getFieldsValue(_fields: Partial<Record<string, Field>>) {
@@ -11,5 +15,17 @@ export function getFieldsValue(_fields: Partial<Record<string, Field>>) {
 
 export function getFieldValue(_f: Field['_f']) {
   const ref = _f.ref;
+  if (_f.refs ? _f.refs.every((ref) => ref.disabled) : ref.disabled) {
+    return;
+  }
+
+  if (_f.refs && isRadioInput(ref)) {
+    return getRadioValue(_f.refs).value;
+  }
+
+  if (_f.refs && isCheckBoxInput(ref)) {
+    return getCheckboxValue(_f.refs).value;
+  }
+
   return ref.value;
 }
