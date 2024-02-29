acd063bb8726273a396442308c065238cdbe8953
seo <seo0h9@gmail.com>
2024-02-29 16:36:17 +0900

    [RMIS-39] remove : create-form-control 불필요 코드 제거

--- src/hooks/use-form/logic/get-field-value.ts
+++ src/hooks/use-form/logic/get-field-value.ts
@@ -1,32 +1,15 @@
 import { isUndefined } from '@/utils/is';
 
-import { isCheckBoxInput, isRadioInput } from '../utils/is';
-
-import { getCheckboxValue } from './get-checkbox-value';
-import { getRadioValue } from './get-radio-value';
 import type { Field } from '../types/fields';
-
-export function getFieldValue(_f: Field['_f']) {
-  const ref = _f.ref;
-
-  if (_f.refs ? _f.refs.every((ref) => ref.disabled) : ref.disabled) {
-    return;
-  }
-
-  if (isRadioInput(ref)) {
-    return getRadioValue(_f.refs).value;
-  }
-
-  if (isCheckBoxInput(ref)) {
-    return getCheckboxValue(_f.refs).value;
-  }
-
-  return ref.value;
-}
 
 export function getFieldsValue(_fields: Partial<Record<string, Field>>) {
   return Object.entries(_fields).reduce(
     (acc, [name, ref]) => ({ ...acc, [name]: isUndefined(ref?._f) ? null : getFieldValue(ref?._f) }),
     {},
   );
+}
+
+export function getFieldValue(_f: Field['_f']) {
+  const ref = _f.ref;
+  return ref.value;
 }
