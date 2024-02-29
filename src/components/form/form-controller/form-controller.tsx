1be6753fd291d9dd0b120611163420644c018971
seo <seo0h9@gmail.com>
2024-02-29 16:35:40 +0900

    [RMIS-39] feat : 질문 뒤로가기 기능 추가

--- src/components/form/form-controller/form-controller.tsx
+++ src/components/form/form-controller/form-controller.tsx
@@ -1,7 +1,8 @@
-import { useCallback } from 'react';
+import { useCallback, useId } from 'react';
 
 import { Button } from '@/components/common/buttons';
 import ErrorMessage from '@/components/common/error/message';
+import { Radio } from '@/components/common/form/radio';
 import { ProgressBar } from '@/components/common/progress-bar/progress-bar';
 
 import { ConditionalInput } from '@/components/form';
@@ -20,11 +21,11 @@ const requiredMessage: LanguagesContents = { ko: '필수 질문입니다.', en: 
 
 export const FormController = () => {
   const { lang } = useLanguageContext();
-  const { isLastQuestion, changeNextQuestion, resetIdx, form, percentage } = useFormQuestionControl();
+  const { forms, isLastQuestion, nextQuestion, beforeQuestion, resetIdx, percentage, currentIdx } =
+    useFormQuestionControl();
   const { onSubmit, submitStatus } = useFormSubmit({ cleanUp: resetIdx });
   const { getFieldState } = useFormContext();
-  const { name, question, required, ...rest } = form;
-  const { invalid, error } = getFieldState(name);
+  const id = useId();
 
   // NOTE: form의 기본 enter 이벤트로 인해 커스텀 enter 이벤트와 겹치는 문제가 있었음.
   // 따라서 form 대신 div 를 사용하고 enter 이벤트의 경우  onsubmit 처리를 해 줌
@@ -32,43 +33,58 @@ export const FormController = () => {
     if (isLastQuestion) {
       onSubmit();
     } else {
-      changeNextQuestion();
+      nextQuestion();
     }
-  }, [changeNextQuestion]);
+  }, [nextQuestion]);
 
   const buttonKey = isLastQuestion ? 'submit' : 'next';
-  const buttonHandler = isLastQuestion ? onSubmit : changeNextQuestion;
+  const buttonHandler = isLastQuestion ? onSubmit : nextQuestion;
   return (
     <>
       {submitStatus.isError && <ErrorTost errorMessages={[{ ko: submitStatus.message, en: submitStatus.message }]} />}
+
       <div className={styles['progress-bar-wrapper']}>
         <ProgressBar visuals={[{ color: globalColor.mainColor, percentage }]} />
       </div>
+      {/* NOTE:  질문이 dom에서 아예 사라지면  이전 답변이 남지 않기에 map을
+          이용해 전체 질문을 펼치고 display를 이용해 보여주는 방식 사용 */}
+      {forms.map((form, idx) => {
+        const { name, question, required, ...rest } = form;
+        const { invalid, error } = getFieldState(name);
 
-      <section className={styles['form-container']}>
-        <label htmlFor={name} className={`${styles['label']} ${required && styles['required']}`}>
-          {question}
-        </label>
+        return (
+          <section
+            key={`${idx}-${id}`}
+            className={styles['form-container']}
+            style={{ display: idx === currentIdx ? 'block' : 'none' }}
+          >
+            <label htmlFor={name} className={`${styles['label']} ${required && styles['required']}`}>
+              {question}
+            </label>
+            <div className={styles['input-wrapper']}>
+              <ConditionalInput
+                key={form.name}
+                selections={form.radioContext}
+                requiredMessage={requiredMessage[lang]}
+                {...rest}
+                {...form}
+              />
 
-        <div className={styles['input-wrapper']}>
-          <ConditionalInput
-            key={form.name}
-            selections={form.radioContext}
-            requiredMessage={requiredMessage[lang]}
-            {...rest}
-            {...form}
-          />
+              <div className={styles['invalid-message-wrapper']}>
+                {invalid && <ErrorMessage error={error?.message} />}
+              </div>
+            </div>
 
-          <div className={styles['invalid-message-wrapper']}>{invalid && <ErrorMessage error={error?.message} />}</div>
-        </div>
-
-        <div className={styles['btn-wrapper']}>
-          <Button key={buttonKey} onClick={buttonHandler}>
-            {isLastQuestion ? 'SUBMIT' : `OK >`}
-          </Button>
-          <PressEnter enterCallback={handleKeyDown} />
-        </div>
-      </section>
+            <div className={styles['btn-wrapper']}>
+              <Button onClick={beforeQuestion} style={{ backgroundColor: `var(--gray-400)` }}>{`< Before`}</Button>
+              <Button key={buttonKey} onClick={buttonHandler}>
+                {isLastQuestion ? 'SUBMIT' : `OK >`}
+              </Button>
+              <PressEnter enterCallback={handleKeyDown} />
+            </div>
+          </section>
+        );
+      })}
     </>
   );
 };
