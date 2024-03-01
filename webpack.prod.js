5a394f1af5417b348fadb03d1643f2af876a761d
seo <seo0h9@gmail.com>
2024-03-01 20:03:01 +0900

    [RMIS-18] chore : webpack config dev, prod 분리

---
+++ webpack.prod.js
@@ -0,0 +1,78 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+const dotenv = require('dotenv');
+const webpack = require('webpack');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+/** @type {import('webpack').Configuration} */
+module.exports = () => {
+  dotenv.config({ path: '.env.dev' });
+
+  return {
+    mode: 'production',
+    devtool: 'source-map',
+    target: 'web',
+    entry: './src/index.tsx',
+
+    output: {
+      path: path.join(__dirname, '/dist'),
+      filename: 'bundle.js',
+      clean: true,
+      publicPath: '/',
+    },
+    resolve: {
+      extensions: ['.ts', '.tsx', '.js', '.jsx'],
+      modules: [path.resolve(__dirname, 'src'), 'node_modules'],
+      alias: {
+        '@': path.resolve(__dirname, './src'),
+      },
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(ts|tsx)$/,
+          exclude: /node_modules/,
+          include: path.resolve(__dirname, 'src'),
+          loader: 'babel-loader',
+        },
+        {
+          test: /\.tsx?$/,
+          loader: 'ts-loader',
+          exclude: /node_modules/,
+          options: {
+            configFile: path.resolve(__dirname, 'tsconfig.json'),
+          },
+        },
+        {
+          test: /\.css$/i,
+          exclude: /\.module\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.module\.css$/i,
+          use: [
+            'style-loader',
+            {
+              loader: 'css-loader',
+              options: {
+                modules: true,
+              },
+            },
+          ],
+        },
+      ],
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: 'public/index.html',
+      }),
+      new webpack.DefinePlugin({
+        'process.env': JSON.stringify(process.env),
+      }),
+      new MiniCssExtractPlugin({
+        filename: 'assets/css/[name].[contenthash:8].css',
+        chunkFilename: 'assets/css/[name].[contenthash:8].chunk.css',
+      }),
+    ],
+  };
+};
