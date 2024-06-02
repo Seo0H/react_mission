7724dd5ce7e4950bd349b5f011e2c1e493916bb7
seo <seo0h9@gmail.com>
2024-06-02 23:00:01 +0900

    [RMIS-48] ui : 스타일 완성도 마무리

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,7 +1,5 @@
 import { Link } from 'react-router-dom';
 
-import { Button } from '@/components/common/buttons';
-
 import { LanguageSelector } from '@/components/lang/lang-selector';
 import { useAuthContext } from '@/hooks/use-auth/auth-context';
 
@@ -22,7 +20,7 @@ const Header = () => {
           <>
             <Link to='/mypage'>내 정보</Link>
             {userInfo?.role === 'admin' && <Link to='/admin'>관리자 페이지</Link>}
-            <Button.SmallNoBg onClick={logout}>로그아웃</Button.SmallNoBg>
+            <button onClick={logout}>로그아웃</button>
           </>
         )}
 
