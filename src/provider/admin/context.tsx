68d7a82be853a72c67999ae839682b77279d8878
seo <seo0h9@gmail.com>
2024-06-07 00:13:04 +0900

    [RMIS-57] fix : 세션 없이 admin page 방문 시 DB 조회하는 로직 방지

--- src/provider/admin/context.tsx
+++ src/provider/admin/context.tsx
@@ -1,6 +1,7 @@
 import { createContext, useContext, useEffect, useState } from 'react';
 
 import { supabase } from '@/api/supabase';
+import { useAuthContext } from '@/hooks/use-auth/auth-context';
 import { Tables } from '@/types/supabase';
 
 type TAdminContext = {
@@ -20,15 +21,24 @@ export const useAdminContext = () => {
 
 export const AdminProvider = ({ children }: { children: React.ReactNode }) => {
   const [formList, setFormList] = useState<Tables<'form'>[] | null>(null);
+  const { session } = useAuthContext();
 
   useEffect(() => {
-    if (!formList)
-      supabase
-        .from('form')
-        .select()
-        .then(({ data, error }) => {
-          if (!error) setFormList(data);
-        });
+    if (!session || formList) return;
+
+    const abortController = new AbortController();
+
+    supabase
+      .from('form')
+      .select()
+      .abortSignal(abortController.signal)
+      .then(({ data, error }) => {
+        if (!error) {
+          setFormList(data);
+        }
+      });
+
+    return () => abortController.abort();
   }, []);
 
   return <AdminContext.Provider value={{ formList }}>{children}</AdminContext.Provider>;
