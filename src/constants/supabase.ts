1ab74e934928b271153c32ee4f2acbfcd62fa7ad
seo <seo0h9@gmail.com>
2024-06-07 16:36:36 +0900

    [RMIS-57] chore : dev mode에 따른 supabase 연결 설정

--- src/constants/supabase.ts
+++ src/constants/supabase.ts
@@ -1,2 +1,9 @@
-export const REACT_APP_SUPABASE_KEY = process.env.REACT_APP_SUPABASE_KEY!;
-export const REACT_APP_SUPABASE_URL = process.env.REACT_APP_SUPABASE_URL!;
+import { isDev } from '@/utils/is';
+
+export const REACT_APP_SUPABASE_KEY = isDev
+  ? process.env.DEV_REACT_APP_SUPABASE_KEY!
+  : process.env.REACT_APP_SUPABASE_KEY!;
+
+export const REACT_APP_SUPABASE_URL = isDev
+  ? process.env.DEV_REACT_APP_SUPABASE_URL!
+  : process.env.REACT_APP_SUPABASE_URL!;
