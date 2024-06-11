74616542aa2a22946a893a862962c50cab398774
seo <seo0h9@gmail.com>
2024-06-11 19:18:46 +0900

    [RMIS-57] refactor : admin provider 구조 리팩터링 및 최적화

--- src/provider/admin/context.tsx
+++ src/provider/admin/context.tsx
@@ -1,11 +1,15 @@
-import { createContext, useContext, useEffect, useState } from 'react';
+import { createContext, useCallback, useContext, useEffect, useState } from 'react';
 
+import { useNavigate } from 'react-router-dom';
+
+import { Form } from '@/api/form/types/server-response';
 import { supabase } from '@/api/supabase';
 import { useAuthContext } from '@/hooks/use-auth/auth-context';
 import { Tables } from '@/types/supabase';
 
 type TAdminContext = {
   formList: Tables<'form'>[] | null;
+  getFormList: (abortSignal?: AbortSignal) => Promise<void>;
 };
 
 const AdminContext = createContext<TAdminContext | null>(null);
@@ -22,29 +26,35 @@ export const useAdminContext = () => {
 export const AdminProvider = ({ children }: { children: React.ReactNode }) => {
   const [formList, setFormList] = useState<Tables<'form'>[] | null>(null);
   const { session } = useAuthContext();
+  const navigator = useNavigate();
 
   useEffect(() => {
-    if (!session || formList) return;
-
     const abortController = new AbortController();
-
-    supabase
-      .from('form')
-      .select()
-      .abortSignal(abortController.signal)
-      .then(({ data, error, status }) => {
-        if (status >= 400) {
-          alert(`폼 목록을 가져오던 중 문제가 발생했습니다. / STATUS : ${status}`);
-          return;
-        }
-
-        if (!error) {
-          setFormList(data);
-        }
-      });
-
+    getFormList(abortController.signal);
     return () => abortController.abort();
   }, []);
 
-  return <AdminContext.Provider value={{ formList }}>{children}</AdminContext.Provider>;
+  const getFormList = useCallback(
+    async (abortSignal?: AbortSignal) => {
+      if (!session) return;
+
+      await supabase
+        .from('form')
+        .select()
+        .abortSignal(abortSignal ?? new AbortController().signal)
+        .then(({ data, error, status }) => {
+          if (status >= 400) {
+            alert(`폼 목록을 가져오던 중 문제가 발생했습니다. / STATUS : ${status}`);
+            return;
+          }
+
+          if (!error) {
+            setFormList(data);
+          }
+        });
+    },
+    [supabase],
+  );
+
+  return <AdminContext.Provider value={{ formList, getFormList }}>{children}</AdminContext.Provider>;
 };
